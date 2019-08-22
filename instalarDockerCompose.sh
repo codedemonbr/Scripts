f12fe8b36f37e0b9466b79e1b146df918150b3e8
Thiago Henrique dos Santos <henrique.thsantos.ths@gmal.com>
2019-08-22 11:11:06 -0300

    Instalação do docker-compose

---
+++ instalarDockerCompose.sh
@@ -0,0 +1,10 @@
+echo "Atualizando"
+sudo apt update && sudo apt upgrade -y
+echo "Instalando curl"
+sudo apt install curl -y
+echo "Baixando docker e jogando no repositorio padrao"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -uname -so /usr/local/bin/docker-compose
+echo "Dando permissao para o docker-compose ser executavel "
+sudo chmod +x /usr/local/bin/docker-compose
+echo "Vendo a versao do docker-compose instalada"
+docker-compose --version
